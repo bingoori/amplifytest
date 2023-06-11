7a3700c050ae27ca9827236049ee6daa1733d13a
sdwoo <ssung@commerceon.co.kr>
2023-06-11 22:19:17 +0900

    add serverside

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,10 +2,13 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
+import { GetServerSideProps } from 'next'
 
 const inter = Inter({ subsets: ['latin'] })
 
-export default function Home() {
+function Home(props: any) {
+  console.log('props ::: ', props)
+  
   return (
     <>
       <Head>
@@ -121,3 +124,13 @@ export default function Home() {
     </>
   )
 }
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  return {
+    props: {
+      context: context,
+    },
+  }
+}
+
+export default Home
